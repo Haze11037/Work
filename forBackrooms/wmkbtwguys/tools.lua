f28795b3be76869b4d2459b05bf8828a6cc9cf4c
Haze11037 <88188104+Haze11037@users.noreply.github.com>
2022-08-28 00:25:37 +0200

    Delete tools.lua

--- forBackrooms/wmkbtwguys/tools.lua
+++
@@ -1,5 +0,0 @@
-{
-  {"Haze11037",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
-  {"Sat_irn",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
-  {"DreamyAegis",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
-}
