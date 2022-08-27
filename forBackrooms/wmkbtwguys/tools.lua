8a6bcc75c8ddfea2a241bd6f4b3a130387d61f1e
Haze11037 <88188104+Haze11037@users.noreply.github.com>
2022-08-28 00:16:51 +0200

    Create tools.lua

---
+++ forBackrooms/wmkbtwguys/tools.lua
@@ -0,0 +1,5 @@
+{
+  {"Haze11037",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
+  {"Sat_irn",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
+  {"DreamyAegis",{"Knife","Mug Kit","Knife","C4","Level ! Tool"}};
+}
