a26d5e3e91f5a83f9f4b286cb65c3ef7dba89604
Haze11037 <88188104+Haze11037@users.noreply.github.com>
2022-08-28 00:22:51 +0200

    Create ranks.lua

---
+++ forBackrooms/wmkbtwguys/ranks.lua
@@ -0,0 +1,42 @@
+{
+  {
+    "Leader",
+    {"Haze11037"}
+  };
+  {
+    "Co-Leader",
+    {"DreamyAegis","Sat_irn"}
+  };
+  {
+    "WMKBTWGUYS I",
+    {}
+  };
+  {
+    "WMKBTWGUYS II",
+    {}
+  };
+  {
+    "WMKBTWGUYS III",
+    {}
+  };
+  {
+    "WMKBTWGUYS IV",
+    {}
+  };
+  {
+    "WMKBTWGUYS V",
+    {}
+  };
+  {
+    "WMKBTWGUYS VI",
+    {}
+  };
+  {
+    "WMKBTWGUYS VII",
+    {"Brotjetimo"}
+  };
+  {
+    "Member",
+    {"RedSaness1","sdafegrshrt","axel_688"}
+  };
+}
