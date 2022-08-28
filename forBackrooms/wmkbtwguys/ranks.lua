625889fa61e6c08ce7e96f2a2147f17d5ac087e3
Haze11037 <88188104+Haze11037@users.noreply.github.com>
2022-08-28 12:24:08 +0200

    Update ranks.lua

--- forBackrooms/wmkbtwguys/ranks.lua
+++ forBackrooms/wmkbtwguys/ranks.lua
@@ -1,39 +1,58 @@
 {
   {
     "Leader",
-    {"Haze11037"}
+    {"Haze11037"},
+    {"Knife","Mug","Level !","C4"}
   };
   {
     "Co-Leader",
-    {"DreamyAegis","Sat_irn"}
+    {"DreamyAegis","Sat_irn"},
+    {"Knife","Mug","Level !","C4"}
   };
   {
     "WMKBTWGUYS I",
-    {}
+    {},
+    {"Level !","Mug"}
+  };
+  {
+    "WMKBTWGUYS I",
+    {},
+    {"C4","Mug"}
+  };
+  {
+    "WMKBTWGUYS I",
+    {},
+    {"Knife","Mug"}
   };
   {
     "WMKBTWGUYS II",
-    {}
+    {},
+    {"Level !"}
   };
   {
     "WMKBTWGUYS III",
-    {}
+    {},
+    {"C4","Mug"}
   };
   {
     "WMKBTWGUYS IV",
-    {"sdafegrshrt"}
+    {"sdafegrshrt"},
+    {"C4"}
   };
   {
     "WMKBTWGUYS V",
-    {}
+    {},
+    {"Knife","Mug"}
   };
   {
     "WMKBTWGUYS VI",
-    {}
+    {},
+    {"Mug"}
   };
   {
     "WMKBTWGUYS VII",
-    {"Brotjetimo"}
+    {"Brotjetimo"},
+    {"Knife"}
   };
   {
     "Member",
